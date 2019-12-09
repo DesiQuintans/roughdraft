c95b5b01344a53b48d0ef5ff4c8bbb45260e1a6d
Desi Quintans <science@desiquintans.com>
2019-12-09 21:04:12 +1100

    Replace program icons

--- compile.bat
+++ compile.bat
@@ -1,1 +1,16 @@
-"C:\Dropbox\Autohotkey\Ahk2Exe_L\Ahk2Exe.exe" /in "RoughDraft-Mode.ahk" /icon "loudspeaker.ico"
+"C:\Dropbox\Autohotkey\Ahk2Exe\Ahk2Exe.exe" /in "RoughDraft-Mode.ahk" /icon "icons\r_blue.ico"
+
+@TIMEOUT 5
+
+
+@REM Main program icon.
+"C:\Dropbox\Apps\ResHacker\ResourceHacker.exe" -open RoughDraft-Mode.exe -save RoughDraft-Mode.exe -action addoverwrite -resource "icons\r_blue.ico" -mask ICONGROUP,159,
+@TIMEOUT 1
+
+
+@REM 'Suspend' icon.
+"C:\Dropbox\Apps\ResHacker\ResourceHacker.exe" -open RoughDraft-Mode.exe -save RoughDraft-Mode.exe -action addoverwrite -resource "icons\r_grey.ico" -mask ICONGROUP,206,
+@TIMEOUT 1
+
+
+@PAUSE
